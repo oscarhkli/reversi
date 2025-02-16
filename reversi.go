af78e06faff9ecf7ab5c144ccee0e6461cd6c8ff
Oscar Li <learning@oscarhkli.com>
2025-02-16 23:41:56 +0000

    feat: add diagonals detection for possibleMoves

--- reversi.go
+++ reversi.go
@@ -1,57 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Point struct {
 	X int
 	Y int
 }
 
-func possibleMoves(board [][]int, self int) map[Point]bool {
-	width := len(board)
-	height := len(board[0])
-
-	p := make(map[Point]bool)
-
-	oppo := 1
-	if self == 1 {
-		oppo = 2
-	}
-
-	dirs := [8][2]int{
-		{-1, 0}, {1, 0}, {0, -1}, {0, 1},
-		{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
-	}
-
-	for y, row := range board {
-		for x, cell := range row {
-			if cell != oppo {
-				continue
-			}
-
-			for _, dir := range dirs {
-				dy, dx := dir[0], dir[1]
-				ny, nx := y+dy, x+dx
-
-				if nx < 0 || nx >= width || ny < 0 || ny >= width || board[ny][nx] != 0 {
-					continue
-				}
-				for j, i := x-dx, y-dy; j >= 0 && j < width && i >= 0 && i <= height; j, i = j-dx, i-dy {
-					if board[j][i] == 0 {
-						break
-					}
-					if board[j][i] == self {
-						p[Point{nx, ny}] = true
-					}
-				}
-			}
-		}
-	}
-
-	return p
+type GameConfig struct {
+	width, height int
+	p1First       bool
+	showHint      bool
 }
 
-func main() {
+type GameBoard struct {
+	cfg     GameConfig
+	board   [][]int
+	turn    int
+	p1Turn  bool
+	p1Score int
+	p2Score int
+}
+
+func NewConfig() GameConfig {
+	return GameConfig{
+		width:    8,
+		height:   8,
+		p1First:  true,
+		showHint: true,
+	}
+}
+
+func NewGameBoard(cfg GameConfig) *GameBoard {
 	board := [][]int{
 		{0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0},
@@ -62,5 +45,98 @@ func main() {
 		{0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0},
 	}
-	fmt.Println(possibleMoves(board, 1))
+	return &GameBoard{
+		cfg:     cfg,
+		board:   board,
+		turn:    1,
+		p1Turn:  cfg.p1First,
+		p1Score: 0,
+		p2Score: 0,
+	}
+}
+
+func (g GameBoard) Print() {
+	var sb strings.Builder
+	line := "+-+-+-+-+-+-+-+-+-+\n"
+
+	sb.WriteString(line)
+	sb.WriteString("| |a|b|c|d|e|f|g|h|\n")
+	sb.WriteString(line)
+	for i, row := range g.board {
+		sb.WriteString(fmt.Sprintf("|%d|", i+1))
+		for _, cell := range row {
+			if cell == 1 {
+				sb.WriteString("●")
+			} else if cell == 2 {
+				sb.WriteString("○")
+			} else {
+				sb.WriteString(" ")
+			}
+			sb.WriteString("|")
+		}
+		sb.WriteString("\n")
+		sb.WriteString(line)
+	}
+	sb.WriteString(fmt.Sprintf("P1: %2d | P2: %2d\n", g.p1Score, g.p2Score))
+
+	fmt.Print(sb.String())
+}
+
+func (g GameBoard) possibleMoves(player int) map[Point][]Point {
+	width, height := g.cfg.width, g.cfg.height
+
+	pMoves := make(map[Point][]Point)
+
+	// If player is 1, oppo is 2; if player is 2; oppo is 1
+	oppo := 1
+	if player == 1 {
+		oppo = 2
+	}
+
+	// Directions - up/down, left/right, diagonals
+	dirs := [8][2]int{
+		{-1, 0}, {1, 0}, {0, -1}, {0, 1},
+		{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
+	}
+
+	for y, row := range g.board {
+		for x, cell := range row {
+			if cell != oppo {
+				continue
+			}
+
+			for _, dir := range dirs {
+				dy, dx := dir[0], dir[1]
+				ty, tx := y+dy, x+dx
+
+				if tx == 0 || tx == width || ty == 0 || ty == height || g.board[ty][tx] != 0 {
+					continue
+				}
+
+				flips := []Point{}
+				for nx, ny := x, y; nx >= 0 && nx < width && ny >= 0 && ny < height; nx, ny = nx-dx, ny-dy {
+					if g.board[ny][nx] == 0 {
+						break
+					}
+					if g.board[ny][nx] == player {
+						p := Point{tx, ty}
+						_, ok := pMoves[p]
+						if !ok {
+							pMoves[p] = flips
+						} else {
+							pMoves[p] = append(pMoves[p], flips...)
+						}
+						break
+					}
+					flips = append(flips, Point{nx, ny})
+				}
+			}
+		}
+	}
+	return pMoves
+}
+
+func main() {
+	g := NewGameBoard(NewConfig())
+	g.Print()
 }
--- reversi_test.go
+++ reversi_test.go
@@ -2,48 +2,112 @@ package main
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
 func TestPossibleMoves(t *testing.T) {
-	board := [][]int{
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 1, 2, 0, 0, 0},
-		{0, 0, 0, 2, 1, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
+	g := GameBoard{
+		cfg: NewConfig(),
+		board: [][]int{
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 1, 2, 0, 0, 0},
+			{0, 0, 0, 2, 1, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+		},
 	}
 
-	if got, want := possibleMoves(board, 1), map[Point]bool{
-		{4, 2}: true,
-		{5, 3}: true,
-		{2, 4}: true,
-		{3, 5}: true,
-	}; !reflect.DeepEqual(got, want) {
+	if got, want := g.possibleMoves(1), map[Point][]Point{
+		{4, 2}: {{4, 3}},
+		{5, 3}: {{4, 3}},
+		{2, 4}: {{3, 4}},
+		{3, 5}: {{3, 4}},
+	}; !equalMapUnorderedSlice(got, want) {
 		t.Errorf("possibleMoves(%v, %v), want: %v, got %v", "board", 1, want, got)
 	}
 }
 
 func TestPossibleMovesForDiagonals(t *testing.T) {
-	board := [][]int{
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 1, 2, 0, 0, 0},
-		{0, 0, 0, 2, 2, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
+	g := GameBoard{
+		cfg: NewConfig(),
+		board: [][]int{
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 2, 0, 0, 0},
+			{0, 0, 0, 1, 2, 1, 0, 0},
+			{0, 0, 0, 2, 2, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+		},
 	}
 
-	if got, want := possibleMoves(board, 1), map[Point]bool{
-		{5, 3}: true,
-		{3, 5}: true,
-		{5, 5}: true,
-	}; !reflect.DeepEqual(got, want) {
+	if got, want := g.possibleMoves(1), map[Point][]Point{
+		{3, 1}: {{4, 2}},
+		{5, 1}: {{4, 2}},
+		{3, 5}: {{3, 4}, {4, 4}},
+		{5, 5}: {{4, 4}},
+	}; !equalMapUnorderedSlice(got, want) {
 		t.Errorf("possibleMoves(%v, %v), want: %v, got %v", "board", 1, want, got)
 	}
+}
+
+func TestPossibleMovesForAllDirections(t *testing.T) {
+	g := GameBoard{
+		cfg: NewConfig(),
+		board: [][]int{
+			{0, 2, 2, 2, 2, 2, 2, 2},
+			{0, 2, 1, 1, 1, 1, 1, 2},
+			{0, 2, 1, 1, 1, 1, 1, 2},
+			{0, 2, 1, 1, 0, 1, 1, 2},
+			{0, 2, 1, 1, 1, 1, 1, 2},
+			{0, 2, 1, 1, 1, 1, 1, 2},
+			{0, 2, 2, 2, 2, 2, 2, 2},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+		},
+	}
+
+	if got, want := g.possibleMoves(2), map[Point][]Point{
+		{4, 3}: {
+			{2, 1}, {4, 1}, {6, 1},
+			{3, 2}, {4, 2}, {5, 2},
+			{2, 3}, {3, 3}, {5, 3}, {6, 3},
+			{3, 4}, {4, 4}, {5, 4},
+			{2, 5}, {4, 5}, {6, 5},
+		},
+	}; !equalMapUnorderedSlice(got, want) {
+		t.Errorf("possibleMoves(%v, %v), want: %v, got %v", "board", 1, want, got)
+	}
+}
+
+func equalMapUnorderedSlice(got, want map[Point][]Point) bool {
+	if len(got) != len(want) {
+		return false
+	}
+	for k, gotV := range got {
+		wantV, ok := want[k]
+		if !ok {
+			return false
+		}
+		sort.Slice(gotV, func(i, j int) bool {
+			if gotV[i].X == gotV[j].X {
+				return gotV[i].Y < gotV[j].Y
+			}
+			return gotV[i].X < gotV[j].X
+		})
+		sort.Slice(wantV, func(i, j int) bool {
+			if wantV[i].X == wantV[j].X {
+				return wantV[i].Y < wantV[j].Y
+			}
+			return wantV[i].X < wantV[j].X
+		})
+		if !reflect.DeepEqual(gotV, wantV) {
+			return false
+		}
+	}
+	return true
 }
